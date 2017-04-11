14e1e96e5250b5a0edb6ea34911e16839d17f120
Dmitry Baranov <dbr@sitecore.net>
2017-04-11 18:32:44 +0300

    no message

---
+++ src/Sitecore.Support.95547/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.95547")]
+[assembly: AssemblyProduct("Sitecore.Support.95547")]
+[assembly: ComVisible(false)]
